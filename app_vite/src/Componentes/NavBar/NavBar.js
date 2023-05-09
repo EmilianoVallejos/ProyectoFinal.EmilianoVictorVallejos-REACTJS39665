4e3b59a716d80c79eeb0bbf8e898167e2062d88e
emilianovallejos1986 <emilianovallejos1986@gmail.com>
2023-05-09 00:43:33 -0300

    segundapreentregaReactRouterDom

--- app_vite/src/Componentes/NavBar/NavBar.js
+++ app_vite/src/Componentes/NavBar/NavBar.js
@@ -9,9 +9,9 @@ const NavBar = () => {
                 <h1 className="header__title">ANIMALUNGO</h1>
             </NavLink>
             <div>
-                <Link to= {"/category/promociones"} className="header__link link">promociones</Link>
-                <Link to= {"/category/alimento balanceado"} className="header__link link">alimento balanceado</Link>
-                <Link to= {"/carrito"} className="header__link link">carrito</Link>
+                <Link to= {"/category/promociones"} className="header__link link">Promociones</Link>
+                <Link to= {"/category/alimento balanceado"} className="header__link link">Alimento Balanceado</Link>
+                <Link to= {"/carrito"} className="header__link link">Carrito</Link>
             </div>
             <CartWidget/>
         </nav>
